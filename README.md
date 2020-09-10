f6cd34543bc5e1607dc6ebac07b860353765b8c5
XORveRCOM <contact@xorver.com>
2020-09-10 15:12:55 +0900

    JSON版ライブラリ対応

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
+
 # strnote
 
 ## 概要
 
 StringNote は Google などにより認証されたユーザが、短文(以下、ノート)を登録し、登録時に払いだされたユーザ識別子(以下、UID)によって第三者が登録されたノートを参照できるものとするサービスです。  
 
+## 利用方法
+
+ブログやホームページにおいては、UID と同時に払いだされた埋め込みタグを HTML 内に埋め込むことでノートの表示を行うことが可能です。  
+JavaScript の知識が必要になりますが、JSON-API を使用するならば、自由に見た目を変更することも可能です。  
+
 ## サービス
 
 URL: [https://strnote.com](https://strnote.com)  
 MAIL: [contact@strnote.com](mailto:contact@strnote.com)  
+
+## ビューア
+
+static/view.html および view2.html は、グループとしたいメンバーのUIDを登録し、API を用いて一覧表示を行うサンプルとなっています。  
+コード内に記述してありますように MIT ライセンスでの参考実装ですので、改造したものを公開することなどは自由です。  
 
 ## ライセンス
 
@@ -15,6 +26,12 @@ MAIL: [contact@strnote.com](mailto:contact@strnote.com)
 
 なお、個々のソースコードの部品に関しては著作権は存在しないという見解です。  
 よって、サンプルとしてのご利用は自由です。  
+
+[english version]  
+This source code is for security verification of StringNote, and is not intended to allow clone sites and the like. Therefore, please note that there is no license.  
+
+In addition, for the individual components of the source code is the view that the copyright does not exist.  
+Therefore, you are free to use them as samples.  
 
 ## 利用パッケージ
 
---
+++ static/notes.js
@@ -0,0 +1,158 @@
+/**
+ * @license MIT
+ */
+
+class Note {
+	parent;
+	uid;
+	name;
+	mes;
+	utc;
+	date;
+	handle;
+	constructor(parent, uid, handle) {
+		this.parent = parent;
+		this.uid = uid;
+		this.handle = handle;
+		this.Refresh();
+		return this;
+	}
+	Refresh() {
+		let note = this;
+		note.getNotePromise(this.uid)
+			.then(function (info) {
+				let obj = JSON.parse(info.result);
+				note.name = obj.name;
+				note.mes = obj.mes;
+				note.utc = obj.utc;
+				note.date = note.local2str(note.utc2local(obj.utc));
+				note.handle(note);
+			}).catch(function (error) {
+				note.parent.onError(error);
+			});
+	}
+	getNotePromise(uid) {
+		return new Promise(function (callback, onerror) {
+			const apiURL = "https://strnote.com/api/v2/";
+			const xhr = new XMLHttpRequest();
+			xhr.open("GET", apiURL + uid, true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4) {
+					const info = {
+						result: xhr.response,
+						request: xhr
+					};
+					switch (xhr.status) {
+						case 200:	// OK
+							try {
+								callback(info);
+							} catch (e) {
+								info.error = e;
+								onerror(info);
+							}
+							break;
+
+						default:
+							onerror(info);
+							break;
+					}
+				}
+			};
+			xhr.send();
+		});
+	}
+	utc2local(ymdhms) {
+		const ye = ymdhms.substring(0, 4);
+		const mo = ymdhms.substring(4, 6) - 1;
+		const da = ymdhms.substring(6, 8);
+		const ho = ymdhms.substring(8, 10);
+		const mi = ymdhms.substring(10, 12);
+		const se = ymdhms.substring(12, 14);
+		return new Date(Date.UTC(ye, mo, da, ho, mi, se));
+	}
+	local2str(dt) {
+		const ye = dt.getFullYear();
+		const mo = "" + (dt.getMonth() + 1);
+		const da = "" + dt.getDate();
+		const ho = "" + dt.getHours();
+		const mi = "" + dt.getMinutes();
+		const se = "" + dt.getSeconds();
+		return ye + "/" + mo.padStart(2, "0") + "/" + da.padStart(2, "0")
+			+ " " + ho.padStart(2, "0") + ":" + mi.padStart(2, "0") + ":" + se.padStart(2, "0");
+	}
+}
+
+class Notes {
+	notes = [];
+	onError = error => { console.log(error); };
+	timer;
+	constructor() {
+		return this;
+	}
+	setInterval(interval) {
+		if (this.timer) {
+			clearInterval(this.timer);
+			this.timer = "";
+		}
+		if (interval && interval>0) {
+			this.timer = setInterval(this.Refresh.bind(this), interval);
+		}
+	}
+	Add(uid, handle) {
+		let note = new Note(this, uid, handle);
+		this.notes.push(note);
+		return note;
+	}
+	Refresh() {
+		let onError = this.onError;
+		let uids = [];
+		for (i in notes) {
+			uids.push(notes[i].uid);
+		}
+		this.listUTCPromise(uids)
+			.then(function (info, result) {
+				let cutcs = JSON.parse(info.result)
+				Object.keys(cutcs).forEach(uid => {
+					let utc = cutcs[uid];
+					let note = notes[uid];
+					if (utc != note.utc) {
+						note.Get();
+					}
+				});
+			}).catch(function (error) {
+				onError(error);
+			});
+	}
+	listUTCPromise(ids) {
+		const apiURL = "https://strnote.com/api/v2/";
+		return new Promise(function (callback, onerror) {
+			const xhr = new XMLHttpRequest();
+			xhr.open("POST", apiURL + "list", true);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4) {
+					const info = {
+						result: xhr.response,
+						request: xhr
+					};
+					switch (xhr.status) {
+						case 200:	// OK
+							try {
+								callback(info);
+							} catch (e) {
+								info.error = e;
+								onerror(info);
+							}
+							break;
+
+						default:
+							onerror(info);
+							break;
+					}
+				}
+			};
+			const form = new FormData();
+			form.append("ids", JSON.stringify(ids));
+			xhr.send(form);
+		});
+	}
+}
