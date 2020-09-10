c2dbf3d27f4f53ea74335d348fa7c6fe27da490d
XORveRCOM <contact@xorver.com>
2020-09-10 09:45:53 +0900

    画面調整

--- static/firebase.js
+++ static/firebase.js
@@ -1,53 +1,53 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyCOr4bemwGStuFyYgk8qrxZe_KUXIIq3X8",
-  authDomain: "stringnote.firebaseapp.com",
-  databaseURL: "https://stringnote.firebaseio.com",
-  projectId: "stringnote",
-  storageBucket: "stringnote.appspot.com",
-  messagingSenderId: "447313251447",
-  appId: "1:447313251447:web:8b7cbd7e514574be1dca72"
+	apiKey: "AIzaSyCOr4bemwGStuFyYgk8qrxZe_KUXIIq3X8",
+	authDomain: "stringnote.firebaseapp.com",
+	databaseURL: "https://stringnote.firebaseio.com",
+	projectId: "stringnote",
+	storageBucket: "stringnote.appspot.com",
+	messagingSenderId: "447313251447",
+	appId: "1:447313251447:web:8b7cbd7e514574be1dca72"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-      return true;
-    },
-    uiShown: function () {
-      // document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // ログイン完了時のリダイレクト先
-  signInFlow: 'popup',
-  signInSuccessUrl: '/',
+	callbacks: {
+		signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+			return true;
+		},
+		uiShown: function () {
+			// document.getElementById('loader').style.display = 'none';
+		}
+	},
+	// ログイン完了時のリダイレクト先
+	signInFlow: 'popup',
+	signInSuccessUrl: '/',
 
-  // 利用する認証機能
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-    // , firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    , firebase.auth.TwitterAuthProvider.PROVIDER_ID
-    , firebase.auth.GithubAuthProvider.PROVIDER_ID
-  ]
+	// 利用する認証機能
+	signInOptions: [
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID
+		// , firebase.auth.FacebookAuthProvider.PROVIDER_ID
+		, firebase.auth.TwitterAuthProvider.PROVIDER_ID
+		, firebase.auth.GithubAuthProvider.PROVIDER_ID
+	]
 };
 
 function getIdTokenPromise() {
-  return firebase.auth().currentUser.getIdToken(true);
+	return firebase.auth().currentUser.getIdToken(true);
 }
 function signOutPromise() {
-  return firebase.auth().signOut();
+	return firebase.auth().signOut();
 }
 
 var onAuthStateChangedHandler = (user) => { }
 
 firebase.auth().onAuthStateChanged((user) => {
-  onAuthStateChangedHandler(user);
+	onAuthStateChangedHandler(user);
 });
 
 function uiStart(id) {
-  // Firebase Auth UI 開始
-  let ui = new firebaseui.auth.AuthUI(firebase.auth());
-  ui.start("#" + id, uiConfig);
+	// Firebase Auth UI 開始
+	let ui = new firebaseui.auth.AuthUI(firebase.auth());
+	ui.start("#" + id, uiConfig);
 }
--- static/getNote.js
+++ static/getNote.js
@@ -4,32 +4,32 @@
  * @license MIT
  */
 function getNotePromise(uid) {
-    return new Promise(function (callback, onerror) {
-        const apiURL = "https://strnote.com/api/v2/";
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", apiURL + uid, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                const info = {
-                    result: xhr.response,
-                    request: xhr
-                };
-                switch (xhr.status) {
-                    case 200:	// OK
-                        try {
-                            callback(info);
-                        } catch (e) {
-                            info.error = e;
-                            onerror(info);
-                        }
-                        break;
+	return new Promise(function (callback, onerror) {
+		const apiURL = "https://strnote.com/api/v2/";
+		const xhr = new XMLHttpRequest();
+		xhr.open("GET", apiURL + uid, true);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState == 4) {
+				const info = {
+					result: xhr.response,
+					request: xhr
+				};
+				switch (xhr.status) {
+					case 200:	// OK
+						try {
+							callback(info);
+						} catch (e) {
+							info.error = e;
+							onerror(info);
+						}
+						break;
 
-                    default:
-                        onerror(info);
-                        break;
-                }
-            }
-        };
-        xhr.send();
-    });
+					default:
+						onerror(info);
+						break;
+				}
+			}
+		};
+		xhr.send();
+	});
 }
